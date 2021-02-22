d61bca429fe5295b2db77f49378d5a24a61294ef
Denis A. Engemann <denis-alexander.engemann@inria.fr>
2021-02-22 13:43:57 +0100

    add missing files

---
+++ meg_feature_extraction/config.py
@@ -0,0 +1,13 @@
+
+import os.path as op
+
+camcan_path = '/storage/store/data/camcan'
+camcan_meg_path = op.join(
+    camcan_path, 'camcan47/cc700/meg/pipeline/release004/')
+camcan_meg_raw_path = op.join(camcan_meg_path, 'data/aamod_meg_get_fif_00001')
+mne_camcan_freesurfer_path = (
+    '/storage/store/data/camcan-mne/freesurfer')
+
+
+# derivative_path = '/storage/store/derivatives/camcan/pipelines/base2018/MEG'
+derivative_path = '/storage/inria/agramfor/camcan_derivatives'
